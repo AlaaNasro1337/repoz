fd80c3c7c531037520e55a70b35bbd835e9cd018
Janfrancisco <janramalho.100@gmail.com>
2018-08-19 21:37:52 -0300

    make it better

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 module.exports = function(application){
     application.get('/', function(req, res){
         //application.app.controllers.index.home(application, req, res);
-        res.render('index', {validacao : {}});
+        //res.render('index', {validacao : {}});
+        var index = require('.app/controllers/index');
+        index();
     });
 };
 
