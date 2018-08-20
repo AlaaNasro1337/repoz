038185ba5f513fc3eb13a9a78c618bd641ba668f
Janfrancisco <janramalho.100@gmail.com>
2018-08-20 19:00:04 -0300

    make it better

--- config/server.js
+++ config/server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 /*   Configurar o middleware  express-validator*/
 app.use(expressValidator());
 
-consign()
+consign({cwd: process.cwd()+"/app"})
     .include('./app/routes')
     .then('./app/controllers')
     .into(app);
