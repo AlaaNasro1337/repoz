ca8f20a1544142175e972867e8a765177e6db486
Janfrancisco <janramalho.100@gmail.com>
2018-08-20 16:37:46 -0300

    make it better

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,9 @@
 module.exports = function(application){
     application.get('/', function(req, res){
-        application.app.controllers.index.home(application, req, res);
+        //application.app.controllers.index.home(application, req, res);
+        var ind = application.app.controllers.index;
+        ind.home(application, req, res);
+        console.log('eehr');
     });
 };
 
