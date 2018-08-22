ff5a34e78f3b374d69ee1563035bc2e8db0ccf59
Janfrancisco <janramalho.100@gmail.com>
2018-08-22 12:43:55 -0300

    make it better

--- config/server.js
+++ config/server.js
@@ -30,8 +30,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(expressValidator());
 
 consign()
-    .include(path.join(__dirname, './app/routes'))
-    .then(path.join(__dirname, './app/controllers'))
+    .include(path.join(__dirname, '/routes'))
+    .then(path.join(__dirname, '/controllers'))
     .into(app);
 
 module.exports = app;
