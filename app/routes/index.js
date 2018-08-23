549908c89b55b1350a3d46b9fb658a9ab2a87ec3
Janfrancisco <janramalho.100@gmail.com>
2018-08-23 19:46:53 -0300

    make it better

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,8 @@
 module.exports = function(application){
     application.get('/', function(req, res){
         //application.app.controllers.index.home(application, req, res);
-        var ind = application.app.controllers.index;
+        //var ind = application.app.controllers.index;
+        var ind = require('./app/controllers/index);
         ind.home(application, req, res);
         console.log('eehr');
     });
